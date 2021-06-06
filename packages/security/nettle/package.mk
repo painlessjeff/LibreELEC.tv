8709b69bc09841a5532efda34ef8884c7784e5f4
painlessjeff <jeff@ownweb.space>
2021-06-06 19:47:41 -0400

    changed nettle repo

--- packages/security/nettle/package.mk
+++ packages/security/nettle/package.mk
@@ -7,7 +7,7 @@ PKG_VERSION="3.7.2"
 PKG_SHA256="8d2a604ef1cde4cd5fb77e422531ea25ad064679ff0adf956e78b3352e0ef162"
 PKG_LICENSE="GPL2"
 PKG_SITE="http://www.lysator.liu.se/~nisse/nettle"
-PKG_URL="http://ftpmirror.gnu.org/gnu/nettle/nettle-${PKG_VERSION}.tar.gz"
+PKG_URL="https://ftp.gnu.org/gnu/nettle/nettle-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain gmp"
 PKG_LONGDESC="A low-level cryptographic library."
 
